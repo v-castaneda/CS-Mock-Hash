5e5adba6830fe1ba4ceb5272c9aa510827bf4c28
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-11-12 13:05:23 -0500

    installed dependencies and updated index.js

--- index.js
+++ index.js
@@ -16,18 +16,22 @@ let globalStore = {}
 // function for checking a password
 checkPassword = async (username, plaintextPassword) => {
     // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
+    // console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
+    
     // Ensure global store contains the user 
     // (this is a quick way to check if an object contains a key)
     if (globalStore[username]) {
         // TODO: Use bcrypt's compare methof to compare a plaintext password to a password hash
+        let result = await bcrypt.compare(plaintextPassword, globalStore[username])
 
         // TODO: The result variable is a boolean. True means the user was valid. Take action accordingly.
         if (result) {
             // TODO: Display message for valid credentials
+            console.log(`\n⭐️ Welcome back, ${username}, you're a valid user! ⭐️\n`)
         }
         else {
             // TODO: Display message for invalid credentials
+             console.log(`🤨 I don't recognize you! Get out! 👋`)
         }
     }
     else {
@@ -38,13 +42,16 @@ checkPassword = async (username, plaintextPassword) => {
 
 hashPassword = async (username, password) => {
     // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
+    // console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
 
     // TODO: Make the password hash using bcrypt
+    let hashedPassword = await bcrypt.hash(password, 12)
 
     // TODO: Add the user and password hash to the global store object
+    globalStore[username] = hashedPassword
 
     // TODO: Print a status update including the username and password hash
+     console.log(`✅ User '${username}' was added. Their password hash is ${hashedPassword}\n`)
 }
 
 
